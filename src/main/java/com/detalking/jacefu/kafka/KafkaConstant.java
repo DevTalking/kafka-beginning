21f0791e630839ccc8aee4deb447a262449facfd
Fuyuxuan <fuyx@xiaoshouyi.com>
2018-12-24 11:58:42 +0800

    Create Kafka constant and create producer properties.

---
+++ src/main/java/com/detalking/jacefu/kafka/KafkaConstant.java
@@ -0,0 +1,8 @@
+package com.detalking.jacefu.kafka;
+
+public class KafkaConstant {
+
+    public static final String BOOTSTRAP_SERVER = "127.0.0.1:9092";
+    public static final String ZOOKEEPER_SERVER = "127.0.0.1:2181";
+
+}
--- src/main/java/com/devtalking/jacefu/kafka/tutorial1/ProducerDemo.java
+++ src/main/java/com/devtalking/jacefu/kafka/tutorial1/ProducerDemo.java
@@ -1,8 +1,20 @@
 package com.devtalking.jacefu.kafka.tutorial1;
 
+import com.detalking.jacefu.kafka.KafkaConstant;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
 
     public static void main(String[] args) {
+
+        // Create producer properties.
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaConstant.BOOTSTRAP_SERVER);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
     }
 
